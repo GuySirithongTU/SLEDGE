61f97910a015410ebe61eb9c5354f58f3554492c
Guy Sirithong <guy_s@sci.tu.ac.th>
2018-09-23 13:06:52 +0700

    Implemented Rotatable script.
    Basic platform rotation now works in three axes.

--- SLEDGE/Assets/Scripts/Rotatable.cs
+++ SLEDGE/Assets/Scripts/Rotatable.cs
@@ -2,26 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotatable : MonoBehaviour {
+public class Rotatable : MonoBehaviour
+{
 
-	private enum axis { x, y, z };
-    [SerializeField] private axis rotationAxis = axis.y;
-    [SerializeField] private float rotationSpeed = 1f;
-    [SerializeField] private float rotationSteps = 180f;
-    
-    private float currentAngle;
-    private bool isRotating = false;
+    // FIELD VARIABLES
+    private enum axis { x, y, z };                              // Axes for field.
+    [SerializeField] private axis rotationAxis = axis.y;        // Axis of rotation.
+    [SerializeField] private float rotationSpeed = 1f;          // Speed of rotation.
+    [SerializeField] private float rotationSteps = 180f;        // Step of rotation in degrees.
+
+    // PRIVATE VARIABLES
+    private bool isRotating = false;        // Rotation coroutine is running.
+    private Vector3 rotationAxisVector;     // Rotation axis as a vector.
 
     private void Awake()
     {
-        UpdateCurrentAngle();
-
-        StartCoroutine(Rotate(true));
+        // Initialize rotationAxisVector.
+        switch (rotationAxis) {
+            case axis.x:
+                rotationAxisVector = Vector3.right; break;
+            case axis.y:
+                rotationAxisVector = Vector3.up; break;
+            case axis.z:
+                rotationAxisVector = Vector3.forward; break;
+            default:
+                rotationAxisVector = Vector3.up; break;
+        }
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.P) && !isRotating) {
+        if (Input.GetKeyDown(KeyCode.P) && !isRotating) {
             StartCoroutine(Rotate(true));
         }
 
@@ -32,48 +43,47 @@ public class Rotatable : MonoBehaviour {
 
     private IEnumerator Rotate(bool directionPositive)
     {
+        // Update rotating status.
         isRotating = true;
 
-        float targetAngle;
+        // Records degrees rotated from the start of the rotation.
+        float degreesRotated = 0.0f;
 
-        if (directionPositive) {
-            targetAngle = currentAngle + rotationSteps;
-        } else {
-            targetAngle = currentAngle - rotationSteps;
-        }
-        /*
-        while(targetAngle < -180.0f || targetAngle > 180.0f) {
-            if(targetAngle < -180.0f) {
-                targetAngle += 360.0f;
-            } else if(targetAngle > 180.0f) {
-                targetAngle -= 360.0f;
+        // Rotate each update while degrees to rotate is less than the rotation step.
+        while (degreesRotated < rotationSteps) {
+
+            // Degrees to rotation this update.
+            float degreesToRotate = (rotationSteps - degreesRotated) * rotationSpeed * Time.deltaTime;
+            degreesToRotate = Mathf.Clamp(degreesToRotate, 1.0f, 360.0f);
+
+            // Rotate the platform.
+            Quaternion rotation;
+            if (directionPositive) {
+                rotation = Quaternion.AngleAxis(degreesToRotate, rotationAxisVector);
+            } else {
+                rotation = Quaternion.AngleAxis(-degreesToRotate, rotationAxisVector);
             }
-        }
-        */
-        Debug.Log(targetAngle);
+            transform.Rotate(rotation.eulerAngles);
 
-        while(Mathf.Abs(targetAngle - currentAngle) > 1f) {
-            transform.Rotate(Vector3.up, (targetAngle - currentAngle) * Mathf.Clamp01(rotationSpeed * Time.deltaTime));
-            UpdateCurrentAngle();
+            // Update degreesRotated.
+            degreesRotated += degreesToRotate;
 
+            // If overshot step, rotate back to destination.
+            if (degreesRotated > rotationSteps) {
+                Quaternion rotationFinal = Quaternion.AngleAxis(degreesRotated - rotationSteps, rotationAxisVector);
+
+                if (directionPositive) {
+                    transform.Rotate(-rotationFinal.eulerAngles);
+                } else {
+                    transform.Rotate(rotationFinal.eulerAngles);
+                }
+            }
+
+            // Wait for next update.
             yield return null;
         }
 
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, targetAngle, transform.eulerAngles.z);
-        UpdateCurrentAngle();
-
+        // Update rotating status.
         isRotating = false;
-    }
-
-    private void UpdateCurrentAngle()
-    {
-        switch (rotationAxis) {
-            case axis.x:
-                currentAngle = transform.eulerAngles.x; break;
-            case axis.y:
-                currentAngle = transform.eulerAngles.y; break;
-            case axis.z:
-                currentAngle = transform.eulerAngles.z; break;
-        }
     }
 }
