c0e731603614506d1959bd424e84e193e8e04d32
Guy Sirithong <guy_s@sci.tu.ac.th>
2018-11-12 21:18:13 +0700

    Added Persisting Level Unlocking,

--- SLEDGE/Assets/Scripts/Miscellaneous/EndOfLevel.cs
+++ SLEDGE/Assets/Scripts/Miscellaneous/EndOfLevel.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class EndOfLevel : MonoBehaviour {
 
     [SerializeField] private GameObject endOfLevelUI;
-    
+
+    [SerializeField] private int currentLevel;
+
     public static bool goalReached;
 
     private void Awake()
@@ -18,6 +23,31 @@ public class EndOfLevel : MonoBehaviour {
         if (collision.CompareTag("Guardian")) {
             endOfLevelUI.SetActive(true);
             goalReached = true;
+
+            SaveLevelLockData();
+        }
+    }
+
+    private void SaveLevelLockData()
+    {
+        if(File.Exists(Application.persistentDataPath + "/levelLockData.dat")) {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream file;
+            LevelLockData data;
+
+            file = File.Open(Application.persistentDataPath + "/levelLockData.dat", FileMode.Open);
+            data = (LevelLockData)binaryFormatter.Deserialize(file);
+            file.Close();
+
+            file = File.Create(Application.persistentDataPath + "/levelLockData.dat");
+
+            if (data.currentLevelUnlocked <= currentLevel) {
+                data.currentLevelUnlocked = currentLevel + 1;
+            }
+
+            binaryFormatter.Serialize(file, data);
+
+            file.Close();
         }
     }
 }
--- SLEDGE/Assets/Scripts/UI/MainMenu.cs
+++ SLEDGE/Assets/Scripts/UI/MainMenu.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class MainMenu : MonoBehaviour {
 
@@ -11,6 +15,21 @@ public class MainMenu : MonoBehaviour {
     [SerializeField] private Transform cameraTarget;
     [SerializeField] private float landingY = 1f;
     [SerializeField] private float levelSelectorY = -3f;
+
+    [SerializeField] private Button[] levelButtons;
+
+    private LevelLockData levelLockData;
+
+    private void Awake()
+    {
+        LoadLevelLockData();
+
+        for(int i = 2; i >= levelLockData.currentLevelUnlocked; i--) {
+            levelButtons[i].interactable = false;
+            levelButtons[i].transform.GetChild(0).gameObject.SetActive(false);
+            levelButtons[i].transform.GetChild(1).gameObject.SetActive(true);
+        }
+    }
 
 	public void LevelSelector()
     {
@@ -43,5 +62,42 @@ public class MainMenu : MonoBehaviour {
         yield return new WaitForSeconds(1.5f);
 
         SceneManager.LoadScene(level);
+    }
+
+    private void LoadLevelLockData()
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream file;
+
+        if (File.Exists(Application.persistentDataPath + "/levelLockData.dat")) {
+            file = File.Open(Application.persistentDataPath + "/levelLockData.dat", FileMode.Open);
+            levelLockData = (LevelLockData)binaryFormatter.Deserialize(file);
+            Debug.Log(levelLockData.currentLevelUnlocked);
+        } else {
+            file = File.Open(Application.persistentDataPath + "/levelLockData.dat", FileMode.OpenOrCreate);
+
+            LevelLockData data = new LevelLockData();
+            binaryFormatter.Serialize(file, data);
+
+            levelLockData = data;
+        }
+
+        file.Close();
+    }
+}
+
+[Serializable]
+public class LevelLockData
+{
+    public int currentLevelUnlocked;
+
+    public LevelLockData()
+    {
+        currentLevelUnlocked = 1;
+    }
+
+    public LevelLockData(int level)
+    {
+        currentLevelUnlocked = level;
     }
 }
